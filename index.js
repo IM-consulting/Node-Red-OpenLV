8e85e69b70231ed663a0813d036a4d5e3c83b5b2
Sion Hughes <sion.w.hughes@gmail.com>
2018-05-22 13:33:48 +0100

    Removed references to keys dir in Dockerfile and index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var env = 'dev';//'prod';
 
 var options = {
   containerId: 'imconsulting_node-red_00',
-  keyPath: './keys/' + env + '/imconsulting_node-red.key',
-  certPath: './keys/' + env + '/imconsulting_node-red.crt',
+  keyPath: './' + env + '/imconsulting_node-red.key',
+  certPath: './' + env + '/imconsulting_node-red.crt',
   rejectUnauthorized: false,//true,
   debug: true
 };
